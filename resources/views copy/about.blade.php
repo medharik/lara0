f6a0085543cbf266507c0e8efa9051c41453c703
medharik <med.harik@gmail.com>
2026-02-26 21:54:31 +0000

    views cpy

---
+++ resources/views copy/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+@section('title')
+    A propos
+@endsection
+@section('content')
+   <h1>A propos</h1>
+@endsection
---
+++ resources/views copy/layout.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title','titre de page par defaut')</title>
+    <style>
+        *{
+            margin: 0;padding: 0;
+        }
+        nav a{
+            color: orangered;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+<nav style="width: 100%;padding: 10px;display: flex;justify-content: center;background: rgba(255, 255, 0, 0.422)">
+    <a href="/accueil">Accueil </a>  <a href="/about?">About</a>
+</nav>
+<main>
+    @yield('content')
+
+</main>
+<footer  style="width: 100%;padding: 10px;position: absolute;bottom: 0;;display: flex;justify-content: center;background: rgba(255, 255, 0, 0.422);">
+    {{ date('Y-m-d') }} OSBT
+</footer>
+</body>
+</html>
